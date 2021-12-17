984ee0ed924a36857ddb7d19b2233dac18c60283
Carlos Rafael de Oliveira Carneiro <contato@carlos.carneiro.nom.br>
2021-12-17 10:40:52 -0300

    First commit

---
+++ src/main/java/br/nom/carneiro/carlos/backend_challenge/BackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.nom.carneiro.carlos.backend_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/nom/carneiro/carlos/backend_challenge/BackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.nom.carneiro.carlos.backend_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
